5f127c1002afd0d0fabfbb5d0d040adc05926519
yogeshwartrehan <yogeshwartrehan@gmail.com>
2018-12-02 19:22:08 +0530

    Simulate promise

--- Mod4Lab/app/labController.js
+++ Mod4Lab/app/labController.js
@@ -1,5 +1,38 @@
 app.controller('labController', [
-    '$scope',
-    function ($scope) {
+    '$scope', '$timeout', '$q',
+    function ($scope, $timeout, $q) {
+        $scope.model = {
+            number: 0,
+            result: 'Ready',
+        };
+        $scope.checkOddNumber = checkOddNumber;
+
+        function checkOddNumber(input) {
+            $scope.model.result = 'Working...';
+            checkOddNumberHandler(input).then(function (result) {
+                $scope.model.result = 'Success: ' + result;
+            }, function (result) {
+                $scope.model.result = 'Error: ' + result;
+            })
+        }
+
+        function checkOddNumberHandler(input) {
+            var defer = $q.defer();
+
+            $timeout(function () {
+                if (isNumberOdd(input)) {
+                    defer.resolve('Yes, an odd number');
+                } else {
+                    defer.reject('Not an odd number');
+                }
+            }, 5000);
+
+            return defer.promise;
+        }
+
+        function isNumberOdd(input) {
+            return !isNaN(input) && input % 2 == 1;
+        }
+
     }
 ]);
