158d2b626a4a9d87c38442ee0221d3545242ff6b
yogeshwar1996 <yogeshwartrehan@gmail.com>
2018-12-08 18:58:01 +0530

    WIP form select tag

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -1,5 +1,15 @@
 app.controller('labController', [
     '$scope',
     function ($scope) {
+        $scope.model = {
+            pizzaOptions: [{disp: 'Cheese Pizza', val: 'cheese'},
+                           {disp: 'Pepperoni Pizza', val: 'pepperoni'},
+                           {disp: 'Margherita Pizza', val: 'margherita'},
+                           {disp: 'BBQ Chicken Pizza', val: 'bbqchiken'},
+                           {disp: 'Combo Pizza', val: 'combo'}
+                ]
+
+        }
+
     }
 ]);
