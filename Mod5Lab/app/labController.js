204355298daf39fff0c56fcabe5a3e971ae42135
yogeshwar1996 <yogeshwartrehan@gmail.com>
2018-12-09 09:16:21 +0530

    Implement submit and reset button

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -2,6 +2,7 @@ app.controller('labController', [
     '$scope',
     function ($scope) {
         $scope.reset = reset;
+        $scope.submit = submit;
         reset();
         $scope.pizzaOptions = [
             {disp: 'Cheese Pizza', val: 'cheese'},
@@ -12,6 +13,9 @@ app.controller('labController', [
         ];
         function reset() {
             $scope.model = {};
+        }
+        function submit(model) {
+            alert('Submitted\n' + JSON.stringify(model, null, 4));
         }
 
     }
