c6f4cc66f6d58f1cb04d09c4548986f77ad3938c
yogeshwar1996 <yogeshwartrehan@gmail.com>
2018-12-09 08:41:44 +0530

    display model properties on view page in json format

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -3,15 +3,13 @@ app.controller('labController', [
     function ($scope) {
         $scope.reset = reset;
         reset();
-        $scope.model = {
-            pizzaOptions: [{disp: 'Cheese Pizza', val: 'cheese'},
-                {disp: 'Pepperoni Pizza', val: 'pepperoni'},
-                {disp: 'Margherita Pizza', val: 'margherita'},
-                {disp: 'BBQ Chicken Pizza', val: 'bbqchiken'},
-                {disp: 'Combo Pizza', val: 'combo'}
-            ]
-        };
-
+        $scope.pizzaOptions = [
+            {disp: 'Cheese Pizza', val: 'cheese'},
+            {disp: 'Pepperoni Pizza', val: 'pepperoni'},
+            {disp: 'Margherita Pizza', val: 'margherita'},
+            {disp: 'BBQ Chicken Pizza', val: 'bbqchiken'},
+            {disp: 'Combo Pizza', val: 'combo'}
+        ];
         function reset() {
             $scope.model = {};
         }
