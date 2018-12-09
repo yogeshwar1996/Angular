9036bab61cf2eda10eb7a94bcc2e99547e9ad2fa
yogeshwar1996 <yogeshwartrehan@gmail.com>
2018-12-09 08:25:18 +0530

    Format code

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -3,13 +3,11 @@ app.controller('labController', [
     function ($scope) {
         $scope.model = {
             pizzaOptions: [{disp: 'Cheese Pizza', val: 'cheese'},
-                           {disp: 'Pepperoni Pizza', val: 'pepperoni'},
-                           {disp: 'Margherita Pizza', val: 'margherita'},
-                           {disp: 'BBQ Chicken Pizza', val: 'bbqchiken'},
-                           {disp: 'Combo Pizza', val: 'combo'}
-                ]
-
+                {disp: 'Pepperoni Pizza', val: 'pepperoni'},
+                {disp: 'Margherita Pizza', val: 'margherita'},
+                {disp: 'BBQ Chicken Pizza', val: 'bbqchiken'},
+                {disp: 'Combo Pizza', val: 'combo'}
+            ]
         }
-
     }
 ]);
